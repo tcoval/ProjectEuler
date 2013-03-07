ea1211ad7284fcc97755bd36deed13c477b05889
Tanner Coval <tannercoval@gmail.com>
2013-03-07 02:25:30 -0800

    Question #13 Completed

---
+++ Questions_1_through_10/Q10_SummationOfPrimes.java
@@ -0,0 +1,19 @@
+package ProjectEuler.Questions_1_through_10;
+
+/*
+ * Question #10
+ * 
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * 
+ * Question: Find the sum of all the primes below two million.
+ * Answer: 
+ */
+
+public class Q10_SummationOfPrimes {
+
+	private static final int MAX = 2000000;
+	
+	public static void main(String[] args) {
+		
+	}
+}
