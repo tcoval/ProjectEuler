d091976c15b12a0cbaa8c2c558a36287a27433f8
Tanner Coval <tannercoval@gmail.com>
2013-03-24 22:16:13 -0700

    Question 21 Completed

--- Questions_21_through_30/Q21_AmicableNumbers.java
+++ Questions_21_through_30/Q21_AmicableNumbers.java
@@ -1,5 +1,6 @@
 package ProjectEuler.Questions_21_through_30;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /*
@@ -12,22 +13,55 @@ import java.util.Set;
  * divisors of 284 are 1, 2, 4, 71, 142; so d(284) = 220.
  * 
  * Question: Evaluate the sum of all the amicable number under 10,000.
- * Answer:
+ * Answer: 31626
  */
 
 public class Q21_AmicableNumbers {
 	
-	private Set<Integer> AmicableNumbers;
+	private static final Set<Integer> AMICABLES = new HashSet<Integer>();
+	private static final int MAX = 10000;
 	
 	public static void main(String[] args) {
-		for (int i = 1; i <= 10000; i++) {
-			int max = (int) Math.round(Math.sqrt(i));
-			int min = (int) Math.round(Math.sqrt(max));
-			int sum = 1 + max;
-			for (int j = min; j <= max; j++) {
-				
+		for (int num = 1; num < MAX; num++) {
+			if (!AMICABLES.contains(num)) {
+				isAmicable(num);
+			}
+		}
+		int totalSum = sumOf(AMICABLES);
+		System.out.println(totalSum);
+	}
+	
+	private static void isAmicable(int n) {
+		Set<Integer> divisors1 = getDivisors(n);
+		int sum1 = sumOf(divisors1);
+		Set<Integer> divisors2 = getDivisors(sum1);
+		int sum2 = sumOf(divisors2);
+		if (sum2 == n && sum1 != n) {
+			AMICABLES.add(n);
+			if (sum1 < MAX) {
+				AMICABLES.add(sum1);
 			}
 		}
 	}
-
+	
+	private static Set<Integer> getDivisors(int n) {
+		Set<Integer> divisors = new HashSet<Integer>();
+		int max = (int) Math.round(Math.sqrt(n));
+		divisors.add(1);
+		for (int i = 2; i <= max; i++) {
+			if (n % i == 0) {
+				divisors.add(i);
+				divisors.add(n/i);
+			}
+		}
+		return divisors;
+	}
+	
+	private static int sumOf(Set<Integer> set) {
+		int sum = 0;
+		for (int i : set) {
+			sum += i;
+		}
+		return sum;
+	}
 }
