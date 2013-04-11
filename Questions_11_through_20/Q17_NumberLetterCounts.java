eb81c7055f968732dd660cab636d55d0f821d62f
Tanner Coval <tannercoval@gmail.com>
2013-04-11 11:41:25 -0700

    Question Format Fixed

--- Questions_11_through_20/Q17_NumberLetterCounts.java
+++ Questions_11_through_20/Q17_NumberLetterCounts.java
@@ -6,16 +6,14 @@ package ProjectEuler.Questions_11_through_20;
  * If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there 
  * are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
  * 
- * If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, 
- * how many letters would be used?
- * 
  * Note: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) 
  * contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of 
  * "and" when writing out numbers is in compliance with British usage.
  * 
  * (My solution is correct for any number up to 1000)
  * 
- * Question: Find the sum of all the primes below two million.
+ * Question: If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, 
+ * how many letters would be used?
  * Answer: 21124
  */
 
